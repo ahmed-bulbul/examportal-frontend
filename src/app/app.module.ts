ec7254b86df7e8f7c3342afb9208fb24dbf95f59
bulbul-deploy <bulbulahmed.cs13@gmail.com>
2021-05-09 08:54:18 +0600

    12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,12 @@ import { LoginComponent } from './pages/login/login.component';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { HomeComponent } from './pages/home/home.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -19,7 +25,8 @@ import {MatIconModule} from '@angular/material/icon';
     NavbarComponent,
     FooterComponent,
     SignupComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +35,12 @@ import {MatIconModule} from '@angular/material/icon';
     MatButtonModule,
     MatInputModule,
     MatFormFieldModule,
-    MatIconModule
+    MatIconModule,
+    FormsModule,
+    HttpClientModule,
+    MatSnackBarModule,
+    MatCardModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserService } from 'src/app/services/user.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +11,85 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService:UserService,private snake:MatSnackBar) { }
+
+  public user={
+    username:'',
+    password:'',
+    firstName:'',
+    lastName:'',
+    email:'',
+    phone:'', 
+
+  }
 
   ngOnInit(): void {
   }
+
+  formSubmit(){
+
+    console.log(this.user);
+
+    //validate
+    if(this.user.username=='' ||this.user.username==null ){
+      // alert('user is required');
+       this.snake.open('Username is required !!','ok',{
+         duration:3000,
+       });
+       return;
+     }
+    if(this.user.password=='' || this.user.password==null){
+      this.snake.open('Password is required !!','ok',{
+        duration:3000,
+      });
+      return;
+    }
+    if(this.user.firstName=='' || this.user.firstName==null){
+      this.snake.open('First Name is required !!','ok',{
+        duration:3000,
+      });
+      return;
+    }
+    if(this.user.lastName=='' || this.user.lastName==null){
+      this.snake.open('Last Name is required !!','ok',{
+        duration:3000,
+      });
+      return;
+    }
+    if(this.user.email=='' || this.user.email==null){
+      this.snake.open('Email is required !!','ok',{
+        duration:3000,
+      });
+      return;
+    }
+    if(this.user.phone=='' || this.user.phone==null){
+      this.snake.open('Phone is required !!','ok',{
+        duration:3000,
+      });
+      return;
+    }
+
+
+    //addUSer: userService
+    this.userService.addUser(this.user).subscribe(
+      (data:any)=>{
+        //success
+        console.log(data);
+        Swal.fire('Successfully registered !!','User id is'+data.id,'success');
+      },
+      (error)=>{
+        //error
+        console.log(error);
+        //alert('something went wrong');
+        this.snake.open('something went wrong !!','ok',{
+          duration:3000,
+        })
+
+      }
+    )
+ 
+  }
+
+  //this.user
 
 }
